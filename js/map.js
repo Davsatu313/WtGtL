4e82f30cfe6bd1ee47883f0e3f1425a07df59123
Davsatu313 <davsatu313@hotmail.com>
2017-03-30 23:19:08 -0500

    first commit

---
+++ js/map.js
@@ -0,0 +1,11 @@
+var map;
+
+//create and draw the map in the mash-up
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 41.8708 , lng: -87.6505},
+    zoom: 18,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  });
+   map.setTilt(45);
+}
