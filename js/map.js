836e057873f3a9b767b59afa0bd6d3f7f080a433
Davsatu313 <davsatu313@hotmail.com>
2017-04-05 20:43:22 -0500

    second iteration

--- js/map.js
+++ js/map.js
@@ -5,7 +5,7 @@ function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 41.8708 , lng: -87.6505},
     zoom: 18,
-    mapTypeId: google.maps.MapTypeId.HYBRID
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   });
    map.setTilt(45);
 }
